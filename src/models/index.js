970eb26ba0530568d4814632eb2d97cb2dba8194
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-09-22 08:09:27 +0530

    # add file

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,3 @@
 module.exports.User = require("./user.model");
 module.exports.Category = require("./category.model");
-module.exports.sub_Category = require("./sub-category.model");
-module.exports.subchildCategory = require("./subchildcategory.model");
-module.exports.Product = require("./product.model");
-module.exports.Cart = require("./cart.model");
-module.exports.Order = require("./order.model");
 module.exports.Token = require("./token.model");
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,8 +1,3 @@
 module.exports.userValidation = require("./user.validation");
 module.exports.categoryValidation = require("./category.validation");
-module.exports.sub_categoryValidation = require("./sub-category.validation");
-module.exports.subchildcategoryValidation = require("./subchildcategory.validation");
-module.exports.productValidation = require("./product.validation");
-module.exports.cartValidation = require("./cart.validation");
-module.exports.orderValidation = require("./order.validation");
 module.exports.tokenValidation = require("./token.validation");
