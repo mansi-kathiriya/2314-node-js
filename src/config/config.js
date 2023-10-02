4f820cb8c776205219b36c6b74160337b45a2f22
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-10-02 09:44:12 +0530

    # config

--- src/config/config.js
+++ src/config/config.js
@@ -10,5 +10,46 @@ const envVarsSchema = Joi.object({
     JWT_SECRET_KRY: Joi.string()
         .description("jwt secreat key")
         .default("this is jwt secreat key"),
-    SMTP_HOST: Joi.string().description("server that will send the emails")
-})
+    SMTP_HOST: Joi.string().description("server that will send the emails"),
+    SMTP_PORT: Joi.number().description("port to connect to theemail server"),
+    SMTP_USERNAME: Joi.string().description("username for email server"),
+    SMTP_PASSWORD: Joi.string().description("password for email server"),
+    EMAIL_FROM: Joi.string().description(
+        "the from field in the emails sent by the app"
+    ),
+}).unknown();
+
+const { value: envVars,error } = envVarsSchema
+    .prefs({ errors: { label: "key" } })
+    .validate(process.env);
+
+if(error){
+    console.log("Config Error: ",error);
+    process.exit(1);
+}
+
+module.exports = {
+    port: envVars.PORT,
+    mongodb: {
+        url: envVars.MONGODB_URL,
+        options: {
+            useNewUrlParse: true,
+            useUnifiedTopology: true,
+        },
+    },
+    base_url: envVars.BASE_URL,
+    jwt: {
+        secret_key: envVars.JWT_SECRET_KRY,
+    },
+    email: {
+        smtp: {
+            host: envVars.SMTP_HOST,
+            port: envVars.SMTP_PORT,
+            auth: {
+                user: envVars.SMTP_USERNAME,
+                pass: envVars.SMTP_PASSWORD,
+            },
+        },
+        from: envVars.EMAIL_FROM,
+    },
+};
