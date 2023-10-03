12da46785ce1993d9737e84d4b8bf52f8c949cc8
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-10-03 09:28:48 +0530

    # validate

--- src/helpers/pick.js
+++ src/helpers/pick.js
@@ -4,9 +4,9 @@
  * @param {string[]} keys
  * @returns {Object}
  */
-const pick = (object,keys) => {
-    return keys.reduce((obj,key) => {
-        if(object && Object.prototype.hasOwnProperty.call(object,key)){
+const pick = (object, keys) => {
+    return keys.reduce((obj, key) => {
+        if (object && Object.prototype.hasOwnProperty.call(object, key)) {
             // eslint-disable-next-line no-param-reassign
             obj[key] = object[key];
         }
