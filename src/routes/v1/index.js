96242a05f3c5fb5c3f2aee25119c7e1c242d29c6
mansi kathiriya <mansikathiriya08@gmail.com>
2023-10-07 08:56:44 +0530

    # token

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRoute = require("./user.route");
+const tokenRoute = require("./token.route");
 
 const router = express.Router();
 
 router.use("/user", userRoute);
+router.use("/token", tokenRoute);
 
 module.exports = router;
